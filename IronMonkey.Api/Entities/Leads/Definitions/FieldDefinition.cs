1d6ee09aa030c7e987d12b079edf3670f82d1089
Manoj Malviya <manoj@girnarsoft.com>
2023-06-23 00:33:21 +0530

    -

--- IronMonkey.Api/Entities/Leads/Definitions/FieldDefinition.cs
+++ IronMonkey.Api/Entities/Leads/Definitions/FieldDefinition.cs
@@ -7,6 +7,8 @@ public class FieldDefinition {
     public string Name { get; set; }
     public FieldType FieldType { get; set; }
 
+    public bool IsRequired { get; set; } = false;
+
     public FieldDefinition(string name, FieldType fieldType) {
         Name = name;
         FieldType = fieldType;
